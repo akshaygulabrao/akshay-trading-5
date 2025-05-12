3b06fb1f30b22f1403b439bed5916fdb10ee01e2
Akshay Gulabrao <aksgula22@gmail.com>
2025-05-12 16:11:55 -0700

    basic script that works, hasn't been tested alot

--- extract_forecast.py
+++ extract_forecast.py
@@ -4,16 +4,29 @@ import pandas as pd
 from utils import get_markets
 import datetime
 from io import StringIO
+from collections import defaultdict
 
 import requests
 
-sites2forecast = {"KLAX": "https://forecast.weather.gov/MapClick.php?lat=33.96&lon=-118.42&lg=english&&FcstType=digital",
-                  "KMIA": "https://forecast.weather.gov/MapClick.php?lat=25.7554&lon=-80.2262&lg=english&&FcstType=digital",
-                  "KDEN": "https://forecast.weather.gov/MapClick.php?lat=39.85&lon=-104.66&lg=english&&FcstType=digital",
-                  "KAUS": "https://forecast.weather.gov/MapClick.php?lat=30.18&lon=-97.68&lg=english&&FcstType=digital",
-                  "KNYC": "https://forecast.weather.gov/MapClick.php?lat=40.78&lon=-73.97&lg=english&&FcstType=digital",
-                  "KPHL": "https://forecast.weather.gov/MapClick.php?lat=40.08&lon=-75.01&lg=english&&FcstType=digital",
-                  "KCHI": "https://forecast.weather.gov/MapClick.php?lat=41.78&lon=-87.76&lg=english&&FcstType=digital"}
+sites2tz = {
+    "KNYC": "America/New_York",
+    "KMDW": "America/Chicago",
+    "KAUS": "America/Chicago",
+    "KMIA": "America/New_York",
+    "KDEN": "America/Denver",
+    "KPHL": "America/New_York",
+    "KLAX": "America/Los_Angeles"
+}
+
+sites2forecast = {
+    "KNYC": "https://forecast.weather.gov/MapClick.php?lat=40.78&lon=-73.97&lg=english&&FcstType=digital",
+    "KMDW": "https://forecast.weather.gov/MapClick.php?lat=41.78&lon=-87.76&lg=english&&FcstType=digital",
+    "KAUS": "https://forecast.weather.gov/MapClick.php?lat=30.18&lon=-97.68&lg=english&&FcstType=digital",
+    "KMIA": "https://forecast.weather.gov/MapClick.php?lat=25.7554&lon=-80.2262&lg=english&&FcstType=digital",
+    "KDEN": "https://forecast.weather.gov/MapClick.php?lat=39.85&lon=-104.66&lg=english&&FcstType=digital",
+    "KPHL": "https://forecast.weather.gov/MapClick.php?lat=40.08&lon=-75.01&lg=english&&FcstType=digital",
+    "KLAX": "https://forecast.weather.gov/MapClick.php?lat=33.96&lon=-118.42&lg=english&&FcstType=digital",
+}
 
 def extract_forecast(site, date):
     """
@@ -29,14 +42,40 @@ def extract_forecast(site, date):
     
     soup = BeautifulSoup(response.text, 'html.parser')
     table = soup.find_all('table')[4]
-    # print(table)
-
+    
+    forecast_dict = defaultdict(list)
     data = []
     for row in table.find_all('tr'):
         row_data = [cell.get_text(strip=True) for cell in row.find_all(['td', 'th'])]
         data.append(row_data)
+    
+    for d in data:
+        if d[0] == '':
+            continue
+        elif d[0] in forecast_dict:
+            forecast_dict[d[0]].extend(d[1:])
+        else:
+            forecast_dict[d[0]] = d[1:]
+    df = pd.DataFrame.from_dict(forecast_dict)
+    df.to_csv("weather_sample.csv",index=None)
+    df = pd.read_csv("weather_sample.csv")
+    df["Date"] = df["Date"].ffill()
+    df["Date"] = df["Date"].apply(lambda x: x + "/25")
+    df["Date"] = pd.to_datetime(df["Date"],format="%m/%d/%y")
+    df["Date"] = df["Date"] + df.iloc[:,1].apply(lambda x: datetime.timedelta(hours=x))
+    df = df.set_index("Date")
 
-    for row in data:
-        print(','.join(row))
+    df_date = df[df.index.date == date]
+    
+    if len(df_date) == 0:
+        return None, None
+    
+    # Get the time of maximum temperature for the specific date
+    max_temp_time = df_date['Temperature (°F)'].idxmax()
+    max_temp = df_date['Temperature (°F)'].max()
+    
+    return max_temp_time, max_temp
 
-extract_forecast("KCHI",datetime.date(2025,5,12))
+
+for site in sites2forecast.keys():
+    print(extract_forecast(site, datetime.date(year=2025,month=5,day=13)))
