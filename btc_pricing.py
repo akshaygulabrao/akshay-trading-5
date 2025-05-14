f74091350b7b1576b6a2460bfbd6749c27cb306d
Akshay Gulabrao <aksgula22@gmail.com>
2025-05-14 01:02:23 -0700

    started work on btc opt pricing

---
+++ btc_pricing.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_json("/home/ox/Downloads/CUR_BTC_prices_download (1).json")
+
+df['log_return'] = np.log(df['close'] / df['close'].shift(1))
+print(df['log_return'].std() * np.sqrt(390))
+
