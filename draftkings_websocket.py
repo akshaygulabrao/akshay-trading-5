f914173a8c07cc9d85bf33ae619e6079ba73b51f
Akshay Gulabrao <aksgula22@gmail.com>
2025-05-14 18:42:09 -0700

    baseball streams, but needs reconnections

--- draftkings_websocket.py
+++ draftkings_websocket.py
@@ -12,10 +12,15 @@ import uuid
 import websockets
 from loguru import logger
 import msgpack
+import zmq
 
 query = {'jsonrpc': '2.0', 'params': {'entity': 'markets', 'queryParams': {'query': "$filter=leagueId eq '84240' and clientMetadata/subCategoryId eq '4519' and tags/all(t: t ne 'SportcastBetBuilder')", 'initialData': False, 'projection': 'betOffers', 'locale': 'en-US'}, 'forwardedHeaders': {}, 'clientMetadata': {'feature': 'LEAGUE_PAGE', 'X-Client-Name': 'web', 'X-Client-Version': '2519.1.1.7'}, 'jwt': 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImN0eSI6IkpXVCJ9.eyJJc0Fub255bW91cyI6IlRydWUiLCJTaXRlR3JvdXBJZCI6IjM5IiwiU2l0ZUlkIjoiMTUwNjUiLCJEb21haW5JZCI6IjQ0OSIsIkNvdW50cnlDb2RlIjoiVVMiLCJuYmYiOjE3NDcyNTMxNTUsImV4cCI6MTc0NzI1MzI3NSwiaWF0IjoxNzQ3MjUzMTU1LCJpc3MiOiJ1cm46ZGsiLCJhdWQiOiJ1cm46ZGsifQ.mPrfLxIzrqF7jRliaFna3cC6A4gg_H_cJV15n8_qIjhbp3cFJYYXa5bZOpv-hnDvjJ7dTi13H-ocpUYxhvyDFKsTJeEZ0-NEYlJwX3TLHwCtiOul56NBcjRbfLRSmW5JOSfX2RNObAvAw6ite72rdl-hXSmx7YrvQMOcjDojIym1uY9Fe8jm9NSnisAS7d4JHYIGWG0gsi5tOy52Tp-YM7Pba8ZhFAdluGdYcEZ9fc99IwuZDh4XWQYOqcd1jJMrmJfvLXIAtnvEGkycKKofwSmsPOfBCZegz4JU9AMMIv-NsQTWvzbhvMLq0FFaISb5ig6svEvjXKGFKmcmxlzTe6R1zI-DERN7umrS_mnRWv-HBPIhh0LDc-DchYGhjx7iGbifkMYs7-BfczQ05jRkRRvsPZ1BG48NhalHcdXRrGjU3mPr7S_17KGA8nAvlhcRxLM-67XqmlU8OE_RN5KE97U_nEyvsBpqawF88GUkG_emKemCL6WMUPcKUeXTISfWlBmEVWcRBA6ErGX3RUWU283k8uZ6sbIFoQvWuf3RQEMxCw0zOIbgo4dh7UPG8XDJHzydOlN6DEvb2i73NcpBKkk4mDU3gNI5EdAqJiSfhihsJ70AMQqFrRpITWC0m9QViUucKiad1rnHMGX_O0pi2SBcH1tJ6QO9g17Vavf0u58', 'siteName': 'dkusnj'}, 'method': 'subscribe', 'id': '4ec8ee8f-ad1c-4aaa-9d2e-9f4a21892a3e'}
 
 ws = None
+context = zmq.Context()
+pub = context.socket(zmq.PUB)
+pub.bind("ipc:///tmp/draftkings_baseball.ipc")
+
 
 async def connect():
     global ws
@@ -41,13 +46,13 @@ async def on_err(e):
     logger.error(e)
 
 async def on_msg(msg):
-    global ws
+    global pub
     msg = msgpack.unpackb(msg)
-    
+    # logger.info(msg)
     # Skip subscription confirmations and non-update messages
     if msg[1] != "update":
         return
-    
+    logger.info(f"{msg[2][2]}")
     # Extract all market updates from the message
     market_updates = msg[2][0][2]  # This gets the list of market updates
     
@@ -69,13 +74,12 @@ async def on_msg(msg):
                     'odds': team_data[3],
                     'decimal_odds': team_data[4]
                 })
-            
-            # Extract timestamps
             ts = msg[2][2]
-            
             t0 = f'{t[0]['name']}:{t[0]['odds']}'
             t1 = f'{t[1]['name']}:{t[1]['odds']}'
-            logger.info(f"Market ID: {m},{t0},{t1},{ts['createdTime']},{ts['publishedTime']}")
+            data = {'market_id' : m , 'teams': t, 'create': ts['createdTime'],'publish':ts['publishedTime']}
+            pub.send_json(data)
+            # logger.info(f"Market ID: {m},{t0},{t1},{ts['createdTime']},{ts['publishedTime']}")
 
 async def main():
     try:
